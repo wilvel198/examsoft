651331ba99e35fd07cd5b19ab703b042001378ef
thewatcher198 <wvelasquezcorp@gmail.com>
2022-12-07 12:14:19 -0600

    updated code so that a file path can be passed through arguments

--- src/main/java/com/examsoft/CountWords.java
+++ src/main/java/com/examsoft/CountWords.java
@@ -12,9 +12,11 @@ import java.util.Map;
 public class CountWords {
 
     public static void main(String[] args) throws IOException {
+        //field used to check if a file path was passed down to the program
         int argsSize = args.length;
-
+        //file location for word processing
         String fileLocation = null;
+        //defalut test case if nothing is passed to the program
         String tempWords = "the cat is in the bag";
 
         //check and see if a file name was passed as a paramter
@@ -36,6 +38,7 @@ public class CountWords {
     // the unique words are counted and a collection is used to put them in reverse order
     // when they print
     public static void countTreeMap(String words2Count) {
+        //wordCounter tree map
         TreeMap<String, Integer> wordCounter
                 = new TreeMap<>(Collections.reverseOrder());
         //the words strings are put in lower case and split into an array for counting
@@ -59,6 +62,7 @@ public class CountWords {
 
     //this method retrieves words from a file to count
     public static String getDataFromFile(String path) throws IOException {
+        //field to hold word counts
         String wordCountInfo = "";
         try {
             FileInputStream fis = new FileInputStream(path);
